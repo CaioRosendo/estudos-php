1633d2d8e8abd1e753b1480ea506da887f2b8f38
Caio Henrique Rosendo da Fonseca <ocaiohenrique@gmail.com>
2022-11-25 22:44:51 -0300

    testanto

--- ola.php
+++ ola.php
@@ -5,11 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        p{
+            font-weight: normal;
+        }
+    </style>
 </head>
 <body>
     <h1> Testando PHP</h1>
     <?php
-        echo "<h2>Cleiton Ama o TOR<br>";
+        echo "<h2>Cleiton Ama o TOR<br> <p> Não fez o curso porque não quis";
     ?>
 </body>
 </html>
