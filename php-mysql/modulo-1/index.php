ab65eee690cdf1876cc17c695bbe6546b8fffe39
Caio Henrique Rosendo da Fonseca <ocaiohenrique@gmail.com>
2023-01-04 19:51:36 -0300

    Criando o index

---
+++ php-mysql/modulo-1/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <title> Listagem de jogos</title>
+</head>
+<body>
+    <div id="corpo">
+        <h1>Escolha seu jogo</h1>
+        <table class="listagem">
+            <tr><td>Foto<td>Nome<td>Adm
+            <tr><td>foto<td>Nome<td>Adm 
+            <tr><td>Foto<td>Nome<td>Adm
+            <tr><td>Foto<td>Nome<td>Adm
+        </table>    
+    </div>
+</body>
+</html>
